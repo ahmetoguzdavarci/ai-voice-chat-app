b403f9bd504471c4b246af25b4cd3dbeb36a95df
ahmetOguz <ahmetoguzdavarci@gmail.com>
2025-09-11 16:20:43 +0300

    - ai model changed

--- app/src/main/java/com/aod/aivoicechat/data/AI_Features.kt
+++ app/src/main/java/com/aod/aivoicechat/data/AI_Features.kt
@@ -1,6 +1,7 @@
 package com.aod.aivoicechat.data
 
-const val MODEL_CHAT_GPT = "gpt-4.1-2025-04-14"
+const val MODEL_CHAT_GPT_5 = "gpt-5-2025-08-07"
+const val MODEL_CHAT_GPT_4_1 = "gpt-4.1-2025-04-14"
 const val TEMPERATURE = 0.4     //between 0-2
 
 const val ROLE_ASSISTANT = "assistant"
--- app/src/main/java/com/aod/aivoicechat/data/model/ChatRequest.kt
+++ app/src/main/java/com/aod/aivoicechat/data/model/ChatRequest.kt
@@ -1,10 +1,10 @@
 package com.aod.aivoicechat.data.model
 
 import androidx.annotation.Keep
-import com.aod.aivoicechat.data.MODEL_CHAT_GPT
+import com.aod.aivoicechat.data.MODEL_CHAT_GPT_5
 
 @Keep
 data class ChatRequest(
-    val model: String = MODEL_CHAT_GPT,
+    val model: String = MODEL_CHAT_GPT_5,
     val messages: List<Message>
 )
